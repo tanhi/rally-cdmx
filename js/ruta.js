436f68a4f42980192c8cff4ca04c8feab5326f6e
carolbaam <carolinabaam@gmail.com>
2018-02-08 12:45:05 -0600

    subiendo cambios

--- js/ruta.js
+++ js/ruta.js
@@ -1,152 +1,90 @@
 var ruta = [
-   {
-      "ponint": 1,
-      "location": {
-         "lat": 19.345842,
-         "lng": -99.190678
-      },
+  
+       {
       "name": "Plaza del Carmen",
-      "info":"¡Bienvenid@ a San Ángel! Declarado Pueblo Típico Pintoresco en 1934 y reconocido, por decreto presidencial, como zona de monumentos históricos en 1987, el pueblo de San Ángel se levanta al sur de la ciudad. Tenanitla fue su nombre prehispánico y significaba “lugar amurallado”, pues estuvo protegido por el pedregal, barrera natural de roca volcánica producto de la erupción del volcán Xitle."
-      "vicinity": "Amargura, Alvaro Obregon, San Ángel TNT, San Ángel, 01000 Ciudad de México, CDMX"
+      "info":"¡Bienvenid@ a San Ángel! Declarado Pueblo Típico Pintoresco en 1934 y reconocido, por decreto presidencial, como zona de monumentos históricos en 1987, el pueblo de San Ángel se levanta al sur de la ciudad. Tenanitla fue su nombre prehispánico y significaba “lugar amurallado”, pues estuvo protegido por el pedregal, barrera natural de roca volcánica producto de la erupción del volcán Xitle.",
+      "vicinity": "Amargura, Alvaro Obregon, San Ángel TNT, San Ángel, 01000 Ciudad de México, CDMX",
       },
-   {
-      "ponint": 2,
-      "location": {
-         "lat": 19.345427,
-         "lng": -99.190976
-      },
+    
+      { 
       "name": "Biblioteca de las Revoluciones de México",
       "info":"Hoy día, el área principal de la Casa de los Dos Patios es ocupada por la Biblioteca de las Revoluciones de México.En el sótano existe un túnel, hoy clausurado. Se cuenta que allí tuvo su guarida el bandido Jesús Arriaga, mejor conocido como Chucho el Roto.",
       "vicinity": "Calle Plaza del Carmen 6, San Ángel TNT, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 3,
-      "location": {
-         "lat": 19.345787,
-         "lng": -99.191766
-      },
+     
+    {
       "name": "Casa del Mayorazgo de Fagoaga",
       "info":"La casona del Mayorazgo Francisco de Fagoaga, quien fue apartador del oro en la Casa de Moneda, es una de las más bellas e imponentes de San Ángel. Fue construida en el siglo XVIII. Se dice que también la habitó el arzobispo Alonso Núñez de Haro y Peralta. Aunque no está abierta al público, se puede visitar para observar su fachada. ",
       "vicinity": "Amargura 15, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 4,
-      "location": {
-         "lat": 19.345247,
-         "lng": -99.193134
-      },
+   
+     {
       "name": "Casa del Obispo",
       "info":"La Casa del Obispo se construyó en 1631. Hoy en día es la Galería de Arte Popular Mexicano, y cuenta con venta de artesanías y joyas mexicanas. Algunos de sus huéspedes distinguidos fueron el presidente Antonio López de Santa Anna y el poeta José Zorrilla. En el siglo XIX fue propiedad del obispo Joaquín Fernández de Madrid y Canal.",
       "vicinity": "Calle Benito Juárez 1, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 5,
-      "place": {
-         "location": {
-            "lat": 19.344696,
-            "lng": -99.193415
-         }
-      },
+   
+      {
       "name": "Parroquia San Jacinto",
       "info":"La construcción de este templo, uno de los más antiguos de la Ciudad de México, inicio a mediados del siglo XVI, al ser fundada una pequeña ermita por los frailes dominicos. En 1580, dicha ermita fue elevada a la categoría de iglesia. En su atrio se puede encontrar una de las primeras cruces labradas en México, en las cuales se mezclan elementos cristianos y paganos interpretados desde el punto de vista y con la tradición artística de los pueblos prehispánicos.",
       "vicinity": "Plaza San Jacinto 18 Bis, Alvaro Obregón, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 6,
-      "location": {
-         "lat": 19.344604,
-         "lng": -99.195254
-      },
-      "name": "Plaza de los cielos",
+     
+      {
+     "name": "Plaza de los cielos",
       "info":"Ahora estás en Plaza de los Cielos.  En la Calle Reina, esquina con la Calle Hermenegildo Galeana, puedes apreciar una de las casas más bellas de la colonia, la cual destaca por una cruz atrial y sus ventanales con herrería y macetones sevillanos.",
       "vicinity": "Calle Benito Juárez 2A, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 7,
-      "location": {
-         "lat": 19.344097,
-         "lng": -99.197042
-      },
+ 
+      {
       "name": "La Casa Blanca",
       "info":"En la Casa Blanca se dice que hay espíritus rondando. Existen diferentes leyendas acerca de su origen. En general todas apuntan a un romance malogrado entre un soldado y una mujer de abolengo que habitaba aquí. En una versión el soldado nunca volvió y la mujer falleció junto a la ventana esperando bajo la luz de la luna. En otras, el soldado regresa para encontrar a su enamorada casada con otro y muere de tristeza. Solo ellos saben la verdad.",
       "vicinity": "01000, Hidalgo 43, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 8,
-      "location": {
-         "lat": 19.343332,
-         "lng": -99.194487
-      },
+  
+      {
       "name": "Plaza de los Arcángeles",
       "info":"Este pequeño lugar es conocido como Plaza de los Arcángeles, aunque su nombre oficial es Plaza del Orgullo Cívico. Data de 1969 como producto del trabajo de los vecinos por embellecer su entorno urbano. Cada banca es de cantera y está dedicada a un arcángel: San Miguel, San Gabriel y San Rafael.",
       "vicinity": "2a Frontera 37, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 9,
-      "location": {
-         "lat": 19.341863,
-         "lng": -99.192487
-      },
+ 
+      {
       "name": "Jardín del Arte",
       "info":"En San Angel hay muchos espacios dedicados al Arte. Este es uno de ellos. Los sábados y domingos varios artistas vienen aquí a exponer sus obras para su venta. Entre semana puedes disfrutar de un tranquilo paseo.",
       "vicinity": "Jardín del Arte 5, San Ángel, 01000 Alvaro Obregon, CDMX"
       },
-   {
-      "ponint": 10,
-      "location": {
-         "lat": 19.344651,
-         "lng": -99.192291
-      },
+   
+      {
       "name": "Plaza San Jacinto",
       "info":"Poca gente sabe que esta plaza es relevante en el ámbito histórico, pues en este sitio es donde en 1847, durante la invasión Yanqui se realizaron las primeras 16 ejecuciones de los valientes 'Héroes Sin Patria' integrantes del Heroico Batallón de San Patricio condecorados con 'Cruz de Honor de la Angostura' por sus acciones en defensa de la soberanía de nuestro país. Por ello en la misma se encuentra el monumento en honor al insigne comandante John Riley y en un costado de la plaza existe una placa conmemorativa del Batallón de San Patricio, misma que lista los nombres de 71 miembros del batallón, 48 de los cuales eran irlandeses y 13 alemanes y Polacos.",
       "vicinity": "San Ángel TNT, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-   "ponint": 11,
-      "location": {
-         "lat": 19.344144, 
-         "lng": -99.192582
-      },
+ 
+      {
       "name": "Plaza Villa San Jacinto",
       "info":"Plaza Villa San Jacinto es una plaza de reciente apertura. El camino de entrada conduce a una casa de campo típica del siglo XIX, completamente restaurada y con un moderno jardín. Las bancas de cantera son un lugar propicio para leer, disfrutar del paisaje o apreciar mejor la arquitectura.",
       "vicinity": "San Jacinto 16, Del. Álvaro Obregón, San Ángel, 01000 Ciudad de México, CDMX"
       },   
-   {
-      "ponint": 12,
-      "location": {
-         "lat": 19.344946,
-         "lng": -99.191952
-      },
+  
+      {
       "name": "Museo Casa del Risco",
       "info":"La muy emblemática fuente del risco con más de 8 metros de altura forma parte del tesoro Artístico de México, fue reconocida el 8 de mayo de 1963 por ser una pieza de arte ultrabarroca mexicana elaborada con pedacería de Concha Nácar real, azulejo, porcelana, platos, tazas, traídas principalmente de China hacia el territorio mexicano que en ese entonces era denominado La Nueva España, dichos pedazos de piezas rotas con los que se construyó la fuente fueron denominados 'riscos' , de los cuales proviene el nombre de la 'fuente del risco' la cual por ser un punto de referencia de la casa hizo que así se le denominara al Museo y a pesar de que se desconoce al autor, se cree que era un arquitecto especializado en el arte ultrabarroco del cual pocos monumentos se tienen actualmente.",
       "vicinity": "Plaza San Jacinto 5, San Ángel TNT, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 13,
-      "location": {
-         "lat": 19.345523,
-         "lng": -99.190378
-      },
-      "name": "Centro Cultural San Ángel",
+  
+      {
+     "name": "Centro Cultural San Ángel",
       "info":"Este edificio fue construido en 1887 para ser el Palacio Municipal. En 1952 fue remodelado para ser la sede de la delegación Álvaro Obregón y fue hasta 1988 que se convirtió en centro cultural. Actualmente alberga un sinúmero de manifestaciones culturales y artísticas.",
       "vicinity": "Alvaro Obregón, Av. Revolución S/n, San Angel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 14,
-      "location": {
-         "lat": 19.345704,
-         "lng": -99.189633
-      },
-      "name": "Templo del Carmen",
+   
+      {
+       "name": "Templo del Carmen",
       "info":"Data de mediados del siglo XVII, Es una joya en San Ángel. La primera misa celebrada en el díasolía realizarse aún de madrugada. Era frecuente que trabajadores y oficinistas entraran brevemente a santiguarse y escuchar unos minutos de misa o solamente con agua bendita. La iglesia tiene una fachada neoclásica y desde los alrededores destacan las cúpulas decoradas con mosaicos de colores.",
       "vicinity": "Av. Revolución sn, San Ángel, 01000 Ciudad de México, CDMX"
       },
-   {
-      "ponint": 15,
-      "location": {
-         "lat": 19.3519663,
-         "lng": -99.1864813
-      },
+  
+     {
       "name": "Museo del Carmen",
       "info":"Ha sido monasterio, colegio para varones y utilizado como cárcel durante la Reforma, hasta convertirse en Museo en 1929. Actualmente el museo se dedica a retratar la historia de la vida cotidiana de la orden de los carmelitas descalzos, así como albergar una pinacoteca de arte novohispano con obras destacadas de Cristóbal de Villalpando y Miguel Cabrera, entre otros.",
       "vicinity": "Calle Plaza del Carmen 4, San Ángel, 01000 Ciudad de México, CDMX"
