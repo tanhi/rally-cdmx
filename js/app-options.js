a3a6bd02942a5fcd0d72c1c00168f00397563d12
carolbaam <carolinabaam@gmail.com>
2018-02-08 19:44:39 -0600

    pintando info

--- js/app-options.js
+++ js/app-options.js
@@ -1,3 +1,11 @@
+function loadPage() {
+	
+	
+	
+	
+}
+
+
 var filterElements = function (e) {
     var selectedIndex = e.target.selectedIndex;
     //manejamos los atributos data
@@ -21,3 +29,42 @@ var filterElements = function (e) {
     paintCard(name, description, vicinity)
 }
 
+ function paintCard(name, description , vicinity){
+
+  var $div = $("<div />", {
+	  "id": "card",
+	  
+  });
+  
+//titulo
+  var $div1 = $("<div />").addClass("row");
+  var $div2 = $("<div />").addClass("col s12 left");
+  var $h5 = $("<h5 />").text(name).addClass("center");
+  //imagen
+  var $div3 = $("<div />").addClass("row");
+  var $div4 = $("<div />").addClass("col s6");
+  var $img = $("<img />");
+  //info
+  var $div5 = $("<div />").addClass("row");
+  var $div6 = $("<div />").addClass("col s12 center");
+  var $p = $("<p />").text(description);
+//appends
+
+$div2.append($h5);
+$div1.append($div2);
+
+$div4.append($img);
+$div3.append($div4);
+
+$div6.append($p);
+$div5.append($div6);
+
+
+$("#card").prepend($div5);
+$("#card").prepend($div1);
+$("#card").prepend($div3);
+
+
+ }
+
+ $(document).ready(loadPage);
