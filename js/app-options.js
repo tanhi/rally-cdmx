d29ae4425b61462a0019f33a271e10bc94d1d127
carolbaam <carolinabaam@gmail.com>
2018-02-08 21:43:37 -0600

    arreglando para imagenes

--- js/app-options.js
+++ js/app-options.js
@@ -21,15 +21,16 @@ var filterElements = function (e) {
     var name= ruta[index]["name"];
     var vicinity=ruta[index]["vicinity"];
     var description=ruta[index]["info"];    
+    var image=ruta[index]["image"];  
     console.log(name);
     console.log(vicinity);
     console.log(description);
     
 
-    paintCard(name, description, vicinity)
+    paintCard(name, description, vicinity, image)
 }
 
- function paintCard(name, description , vicinity){
+ function paintCard(name, description , vicinity, image){
 
   var $div = $("<div />", {
 	  "id": "card",
@@ -42,21 +43,24 @@ var filterElements = function (e) {
   var $h5 = $("<h5 />").text(name).addClass("center");
   //imagen
   var $div3 = $("<div />").addClass("row");
-  var $div4 = $("<div />").addClass("col s6");
-  var $img = $("<img />");
+  var $div4 = $("<div />").addClass("col s12");
+  var $image = $("<img />").attr('src', image).addClass("responsive-image");
   //info
   var $div5 = $("<div />").addClass("row");
   var $div6 = $("<div />").addClass("col s12 center");
   var $p = $("<p />").text(description);
+  var $p2 = $("<p />").text(vicinity);
 //appends
 
 $div2.append($h5);
 $div1.append($div2);
 
-$div4.append($img);
+$div4.append($image);
 $div3.append($div4);
 
 $div6.append($p);
+$div6.append($p2);
+
 $div5.append($div6);
 
 
